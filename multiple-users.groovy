56eadf9a81f2d6a3edc979f4d3b0dcd3a6660d3b
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-03-18 17:37:23 -0400

    add multiple-users missing fields validation

--- multiple-users.groovy
+++ multiple-users.groovy
@@ -1,31 +1,41 @@
-def userExists(username) {
-    def existingUser = jenkins.model.Jenkins.instance.getUser(username)
-    return existingUser != null
+node {
+    stage('Create Jenkins User') {
+        def userExists(username) {
+            def existingUser = jenkins.model.Jenkins.instance.getUser(username)
+            return existingUser != null
+        }
+
+        def username = ''
+        def password = ''
+
+        while (username.isEmpty() || password.isEmpty() || userExists(username)) {
+            stage('User Input') {
+                def userInput = input(
+                    id: 'userInput',
+                    message: 'Introduce a username and a password',
+                    parameters: [
+                        [$class: 'TextParameterDefinition', defaultValue: '', description: 'Insert a username', name: 'username'],
+                        [$class: 'TextParameterDefinition', defaultValue: '', description: 'Insert a password', name: 'password']
+                    ])
+
+                username = userInput['username']
+                password = userInput['password']
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    echo "Username and password are required fields and cannot be empty."
+                } else if (userExists(username)) {
+                    echo "User '${username}' already exists. Please choose a different username."
+                }
+            }
+        }
+
+        def instance = jenkins.model.Jenkins.instance
+        def hudsonRealm = instance.getSecurityRealm()
+
+        def newUser = hudsonRealm.createAccount(username, password)
+        newUser.save()
+
+        echo "Jenkins user '${username}' created successfully."
+    }
 }
 
-def userInput = input(
-    id: 'userInput',
-    message: 'Introduce a username and a password',
-    parameters: [
-        [$class: 'TextParameterDefinition', defaultValue: '', description: 'Insert a username', name: 'username'],
-        [$class: 'TextParameterDefinition', defaultValue: '', description: 'Insert a password', name: 'password']
-    ])
-
-def username = userInput['username']
-def password = userInput['password']
-
-if (username.isEmpty() || password.isEmpty()) {
-    error("Username and password are required fields.")
-}
-
-def instance = jenkins.model.Jenkins.instance
-def hudsonRealm = instance.getSecurityRealm()
-
-if (userExists(username)) {
-    echo "User '${username}' already exists."
-} else {
-    def newUser = hudsonRealm.createAccount(username, password)
-    newUser.save()
-
-    echo "Jenkins user '${username}' created successfully."
-}
