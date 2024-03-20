1de157d4a245cc21d5a16996a88227e0b2d7393a
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-03-20 15:55:05 -0400

    update remove-files to remove all inside the path

--- remove-files.groovy
+++ remove-files.groovy
@@ -2,10 +2,9 @@ pipeline {
     agent any
 
     stages {
-        stage('Delete Unnecessary Files') {
+        stage('Delete All Files in Directory') {
             steps {
                 script {
-                    // Prompt user for directory path
                     def userInput = input(
                         id: 'directoryInput',
                         message: 'Enter the directory path to clean:',
@@ -13,18 +12,15 @@ pipeline {
                             [$class: 'TextParameterDefinition', defaultValue: '', description: 'Directory Path', name: 'directoryPath']
                         ])
 
-                    // Get directory path from user input
                     def directoryPath = userInput['directoryPath']
 
-                    // Check if directory path is empty
                     if (directoryPath.isEmpty()) {
                         error("Directory path cannot be empty.")
                     }
 
-                    // Logic to delete unnecessary files using auto-remove
-                    sh "find ${directoryPath} -name '*.tmp' -auto-remove"
-                    
-                    echo "Unnecessary files deleted successfully in ${directoryPath}."
+                    sh "rm -rf ${directoryPath}/*"
+
+                    echo "All files inside ${directoryPath} have been deleted successfully."
                 }
             }
         }
